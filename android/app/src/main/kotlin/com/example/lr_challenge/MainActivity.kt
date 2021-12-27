940801f4f5d5b05d36c9147bdaa82f87115bd22a
Thássio Vinícius Costa <thassio.vinicius.costa@gmail.com>
2021-12-27 20:03:19 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/lr_challenge/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lr_challenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: ReNest()));
+}
+
+class ReNest extends StatelessWidget {
+  const ReNest({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
